3367ebf15ceb8bb2c6b92b0a23e16c4661bcd71b
morales04 <andres.4moscoso@gmail.com>
2026-04-30 21:49:55 -0500

    creacion de excepciones ejercicio sistema de stock

---
+++ ejercicio sistema de stock/excepciones/excepcion_inventario.py
@@ -0,0 +1,24 @@
+from .base import InventarioException
+
+
+class StockInsuficienteError(InventarioException):
+    def __init__(self, producto, solicitado, disponible):
+        self.producto = producto
+        self.solicitado = solicitado
+        self.disponible = disponible
+        mensaje = f"Stock insuficiente para {producto}. Solicitado: {solicitado}, Disponible: {disponible}"
+        super().__init__(mensaje)
+
+
+class ProductoNoEncontradoError(InventarioException):
+    def __init__(self, producto):
+        self.producto = producto
+        mensaje = f"Producto '{producto} no encontrado en el inventario"
+        super().__init__(mensaje)
+
+
+class CategoriaInvalidaError(InventarioException):
+    def __init__(self, categoria):
+        self.categoria = categoria
+        mensaje = f"Categoria '{categoria} no es valida"
+        super().__init__(mensaje)
